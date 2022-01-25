24780fff5c0835ca3349bfe8e780844985da03ab
Berenguer Pou <berenguer.pou@gmail.com>
2022-01-25 20:45:37 +0100

    fixed custom properties getter

--- js/Utilities/getIndividualProperties.js
+++ js/Utilities/getIndividualProperties.js
@@ -1,9 +1,17 @@
+import Luchador from "../Classes/Luchador.js";
+import Asesor from "../Classes/Asesor.js";
+
 const getIndividualProperties = (character) => {
   const customProperties = Object.getOwnPropertyNames(character);
   const inheritedProperties = ["nombre", "familia", "edad", "vivo", "serie"];
-  return customProperties.filter(
-    (property) => !inheritedProperties.includes(property)
-  );
+  return customProperties
+    .filter((property) => !inheritedProperties.includes(property))
+    .map((property) => {
+      if (property instanceof Luchador || property instanceof Asesor) {
+        return { label: property, value: character[property].nombre };
+      }
+      return { label: property, value: character[property] };
+    });
 };
 
 export default getIndividualProperties;
